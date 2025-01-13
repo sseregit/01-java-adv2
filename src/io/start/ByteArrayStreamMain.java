6f3ecf4a1affeb9f47b8aac6865cc2a818262187
장연식 <sseregit@knowwheresoft.com>
2025-01-13 13:46:13 +0900

    InputStream, OutputStream

---
+++ src/io/start/ByteArrayStreamMain.java
@@ -0,0 +1,21 @@
+package io.start;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class ByteArrayStreamMain {
+
+    public static void main(String[] args) throws IOException {
+        byte[] input = {1, 2, 3};
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        baos.write(input);
+        baos.close();
+
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        byte[] bytes = bais.readAllBytes();
+        System.out.println(Arrays.toString(bytes));
+    }
+}
