aa530160f956835fdabf5ec02e339d63bf95eb13
장연식 <sseregit@knowwheresoft.com>
2025-01-13 15:27:06 +0900

    파일 입출력과 성능 최적화5 - 한 번에 쓰기

--- src/io/buffered/ReadFileV4.java
+++ src/io/buffered/ReadFileV4.java
@@ -5,22 +5,19 @@ import java.io.IOException;
 
 import static io.buffered.BufferedConst.FILE_NAME;
 
-public class ReadFileV1 {
+public class ReadFileV4 {
 
     public static void main(String[] args) throws IOException {
         FileInputStream fis = new FileInputStream(FILE_NAME);
 
         long startTime = System.currentTimeMillis();
 
-        int fileSize = 0;
-        int data;
-        while ((data = fis.read()) != -1) {
-            fileSize++;
-        }
+        byte[] bytes = fis.readAllBytes();
         fis.close();
+
         long endTime = System.currentTimeMillis();
         System.out.println("File name: " + FILE_NAME);
-        System.out.println("File size: " + fileSize / 1024 / 1024 + "MB");
+        System.out.println("File size: " + bytes.length / 1024 / 1024 + "MB");
         System.out.println("Time taken: " + (endTime - startTime) + "ms");
     }
 }
