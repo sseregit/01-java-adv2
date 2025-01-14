8c659238f73c8e6e75d65d2217ce59c521379ab3
장연식 <sseregit@knowwheresoft.com>
2025-01-14 18:07:25 +0900

    네트워크 프로그램1 - 예제

---
+++ src/network/tcp/ClientV1.java
@@ -0,0 +1,33 @@
+package network.tcp;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import static util.MyLogger.log;
+
+public class ClientV1 {
+
+    private static final int PORT = 12345;
+
+    public static void main(String[] args) throws IOException {
+        log("클라이언트 시작");
+        Socket socket = new Socket("localhost", PORT);
+
+        DataInputStream input = new DataInputStream(socket.getInputStream());
+        DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+        log("소켓 연결: " + socket);
+        String toSend = "Hello";
+        output.writeUTF(toSend);
+        log("client -> server: " + toSend);
+
+        String received = input.readUTF();
+        log("client <- server: " + received);
+
+        log("연결 종료: " + socket);
+        input.close();
+        output.close();
+        socket.close();
+    }
+}
