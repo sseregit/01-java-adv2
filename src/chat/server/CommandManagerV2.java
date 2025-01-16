52c2f3f2c54756c9dbb685d565b2fa405e40f4b6
장연식 <sseregit@knowwheresoft.com>
2025-01-16 17:04:40 +0900

    채팅 프로그램 - 서버2

---
+++ src/chat/server/CommandManagerV2.java
@@ -0,0 +1,45 @@
+package chat.server;
+
+import java.io.IOException;
+import java.util.List;
+
+public class CommandManagerV2 implements CommandManager {
+
+    private static final String DELIMETER = "\\|";
+    private final SessionManager sessionManager;
+
+    public CommandManagerV2(SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
+    }
+
+    @Override
+    public void execute(String totalMessage, Session session) throws IOException {
+        if (totalMessage.startsWith("/join")) {
+            String[] split = totalMessage.split(DELIMETER);
+            String username = split[1];
+            session.setUsername(username);
+            sessionManager.sendAll(username + "님이 입장했습니다.");
+        } else if (totalMessage.startsWith("/message")) {
+            String[] split = totalMessage.split(DELIMETER);
+            String message = split[1];
+            sessionManager.sendAll("[" + session.getUsername() + "] " + message);
+        } else if (totalMessage.startsWith("/change")) {
+            String[] split = totalMessage.split(DELIMETER);
+            String changeName = split[1];
+            sessionManager.sendAll(session.getUsername() + "님이 " + changeName + "로 이름을 변경했습니다.");
+            session.setUsername(changeName);
+        } else if (totalMessage.startsWith("/users")) {
+            List<String> usernames = sessionManager.getAllUsername();
+            StringBuilder sb = new StringBuilder();
+            sb.append("전체 접속자 : ").append(usernames.size()).append("\n");
+            for (String username : usernames) {
+                sb.append(" - ").append(username).append("\n");
+            }
+            session.send(sb.toString());
+        } else if (totalMessage.startsWith("/exit")) {
+            throw new IOException("exit");
+        } else {
+            session.send("처리할 수 없는 명령어 입니다: " + totalMessage);
+        }
+    }
+}
