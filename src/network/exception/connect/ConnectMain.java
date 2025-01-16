27614fe710f7d1df0733821fe9260b9475955dd8
장연식 <sseregit@knowwheresoft.com>
2025-01-16 10:45:18 +0900

    네트워크 예외1 - 연결 예외

---
+++ src/network/exception/connect/ConnectMain.java
@@ -0,0 +1,40 @@
+package network.exception.connect;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ConnectMain {
+
+    public static void main(String[] args) throws IOException {
+        unKnownHostEx1();
+        unKnownHostEx2();
+        connectionRefused();
+    }
+
+    private static void unKnownHostEx1() throws IOException {
+        try {
+            Socket socket = new Socket("999.999.999.999", 80);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void unKnownHostEx2() throws IOException {
+        try {
+            Socket socket = new Socket("google.gogo", 80);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void connectionRefused() throws IOException {
+        try {
+            Socket socket = new Socket("localhost", 45678);
+        } catch (ConnectException e) {
+            e.printStackTrace();
+        }
+    }
+}
